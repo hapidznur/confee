7db853dd2268229a53867e2c8ef7885432c8856e
hafid hidayatullah <hafid@samudrainovasi.com>
2024-07-03 18:18:24 +0700

    init config

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+-- Load all mod
+local load = function(mod)
+  package.loaded[mod] = nil
+end
+
+-- Load Folder
+require('user.settings')
+require('user.commands')
+require('user.keymaps')
+
+-- Load plugins
+require('user.plugins')
+
+-- Load colorscheme
+pcall(vim.cmd.colorscheme, 'tokyonight')
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,12 @@
+
+local Plugins = {
+  {'tpope/vim-repeat'},
+  {'tpope/vim-fugitive'},
+  {'stevearc/dressing.nvim', opts = {}},
+  {'nvim-tree/nvim-web-devicons', lazy = true},
+
+  -- colorscheme
+  {'folke/tokyonight.nvim'},
+}
+
+return Plugins
